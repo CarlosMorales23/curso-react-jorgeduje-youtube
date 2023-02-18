13d9b2f34787cf32c68e65a2be6852c331ddd656
Carlos Morales <carlosmora230190@gmail.com>
2023-02-18 00:18:54 -0300

    primeros-componentes-common

---
+++ src/components/common/mainHome/MainHome.jsx
@@ -0,0 +1,17 @@
+const MainHome = ( {nombre, apellido}) =>{
+    // console.log(props.Nombre);
+
+    <>
+        <div>
+            <h2> Hola desde el home </h2>
+
+            <h4>{nombre} {apellido}</h4>
+        </div>
+
+        <div>
+            <h3>Hola</h3>
+        </div>
+    </>
+}
+
+export default MainHome 
---
+++ src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () =>{
+    <ul>
+        <li>Inicio</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+    </ul>
+}
+
+export default Navbar
